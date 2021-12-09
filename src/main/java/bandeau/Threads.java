cfbba601a0c2508917e067a1fc3516d86e491ef4
DenoelaGuennoc <dguennoc@gmail.com>
2021-12-09 09:30:45 +0100

    .

--- src/main/java/bandeau/Threads.java
+++ src/main/java/bandeau/Threads.java
@@ -17,10 +17,11 @@ class Threads extends Thread{
     public void run() {
         // Equivalent de “main” pour un thread
         synchronized(monBandeau){
-        for (ScenarioElement element : myElements) {
-            for (int repeats = 0; repeats < element.repeats; repeats++) {
-                element.effect.playOn(monBandeau);
+            for (ScenarioElement element : myElements) {
+                for (int repeats = 0; repeats < element.repeats; repeats++) {
+                    element.effect.playOn(monBandeau);
+                }
             }
-        }}
+        }
     }
 }
