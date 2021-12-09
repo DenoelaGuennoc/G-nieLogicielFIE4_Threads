157762143f35eec9fcc625d99f36ce508d04ceb9
DenoelaGuennoc <dguennoc@gmail.com>
2021-12-09 09:16:47 +0100

    Exécution parallèle des 3 bandeaux

---
+++ src/main/java/bandeau/Threads.java
@@ -0,0 +1,25 @@
+package bandeau;
+
+import java.lang.reflect.Constructor;
+import java.util.LinkedList;
+import java.util.List;
+
+class Threads extends Thread{
+    
+    private List<ScenarioElement> myElements = new LinkedList<>();
+    private Bandeau monBandeau;
+    
+    public Threads(Bandeau b, List<ScenarioElement> pList) {
+        this.monBandeau = b;
+        this.myElements = pList;
+    }
+
+    public void run() {
+        // Equivalent de “main” pour un thread
+        for (ScenarioElement element : myElements) {
+            for (int repeats = 0; repeats < element.repeats; repeats++) {
+                element.effect.playOn(monBandeau);
+            }
+        }
+    }
+}
